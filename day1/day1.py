7e96acc4ff2bea6a759cc8cf07918b61e1f2e8c6
jluc6628 <jayden6628@gmail.com>
2025-12-02 22:17:38 -0700

    finish day1.py

---
+++ day1/day1.py
@@ -0,0 +1,23 @@
+def main():
+    code = 0
+    num = 50
+    with open("day1/day1.txt","r") as file:
+        lines = file.readlines()
+        for line in lines:
+            if line[0] == "R":
+                line = line[1:]
+                num += int(line)
+            else:
+                line = line[1:]
+                num -= int(line)
+
+            num = num% 100
+            if num == 0:
+                code += 1
+
+    print(code)
+    pass
+
+
+if __name__ == "__main__":
+    main()
